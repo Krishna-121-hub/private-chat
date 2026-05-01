b6efabbf9e1569b70715d65cf03dd3e933c418dd
Krishna-121-hub <krishan.7037225587@gmail.com>
2026-05-02 03:06:08 +0530

    serve root index file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const server = http.createServer(app);
